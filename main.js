f76e4de8a2c94bf5968d9ed45dda87946fb67a31
abelRdimby <40042hiuminess@gmail.com>
2023-08-11 12:40:53 +0200

    icon insered

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 
-  
+  // var anana = 'maintso be mihintsy ';
+  // console.log(anana)
 fetch("https://jsonplaceholder.typicode.com/users")
 
-    .then ((response) => response.json())
-  .then(data => {
-    data.forEach(function(value , index, array) {
-        console.log(data)
-        document.getElementById('tableau').innerHTML += `
-        <tr>
-            <th style="text-align: left"><p>${data[index].name}</p></th>
-            <th style="text-align: left"><p>${data[index].username}</p></th>
-            <th style="text-align: left"><p>${data[index].email}</p></th>
-        </tr>
-        ` 
+.then ((response) => response.json())
+.then(data => {
+data.forEach(function (element, index, array) {
+    // console.log(data)
+    document.getElementById('tableau').innerHTML += `
+    <tr>
+        <th style="text-align: left"><p>${data[index].name}</p></th>
+        <th style="text-align: left"><p>${data[index].username}</p></th>
+        <th style="text-align: left"><p>${data[index].email}</p></th>
+    </tr>
+    ` 
     })  
-  } 
-    
-          
+  }       
 )
 
 
