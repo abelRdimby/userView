c19fd2baa77d69dc65914745adac12a69c124cfc
abelRdimby <40042hiuminess@gmail.com>
2023-12-14 23:59:01 +0100

    json-server Okay

--- mainAxiosUSe.js
+++ mainAxiosUSe.js
@@ -3,18 +3,19 @@
 getData();
 function getData() {
     axios.get(
-        'https://jsonplaceholder.typicode.com/users'
+        'http://localhost:3000/users'
         )
  .then((response) => {
+    console.log(response.data)
     response.data.forEach(function (element, index, array){
-        // console.log(element)
+        console.log(element, index)
         document.getElementById('tableau').innerHTML += `
 
         
         <div class="table">
             <div class="nameBlock">
-                <p id="username">${element.username}</p>
-                <p id="name">${element.name}</p>
+                <p id="username">${element.firstname}</p>
+                <p id="name">${element.lastname}</p>
             </div>
             <div class="imageAvatar">
                 <img src="src/img/Avatar.png" alt="">
@@ -27,65 +28,92 @@ function getData() {
         `
     });
  })
+
+// axios.get(
+//     'http://localhost:3000/users'
+// )
+// .then (Response => {
+//     console.log(Response.data)
+// })
+// .then(response => {
+//     // La réponse de l'API est dans response.data (pas besoin de response.data si l'API renvoie directement un objet)
+//     const apiResponse = response.data;
+
+//     // Maintenant, vous pouvez accéder aux propriétés de l'objet
+//     console.log(apiResponse);
+
+//     Object.entries(apiResponse).forEach((element, index, array) => {
+//         console.log(element)
+//     })
+
+//     // Par exemple, pour accéder à une propriété spécifique
+//     console.log(apiResponse.name);
+//   })
  .catch((error) => console.log(error));
+}
+
+selectElement()
+function selectElement() {
+    var elementSelectioned = document.querySelector(".table").nodeValue
+    console.log(elementSelectioned)
 }
 const block = document.querySelector('#block')
 const App = document.getElementById('myApp')
 const modifBtn = document.getElementById('modif')
-function removeElement() {
-    block.remove()  
-    addNewBlock()
-    console.log('voafafa')
-}
+// function removeElement() {
+//     block.remove()  
+//     addNewBlock()
+//     console.log('voafafa')
+// }
 
-function createElement() {
-    const myBlock = document.createElement('div')
-    myBlock.classList.add('userBlock')
-    document.append.append(myBlock)
-    console.log('misia')
-}
-modifBtn.addEventListener('click',createElement)
-function addNewBlock() {
-    const title = document.getElementById('myTitle').innerHTML ='Add a new users ?'
-    document.getElementById('myApp').innerHTML += `
-        <div class="useradd" id="signUp" style="transition: all 0.9s;">
-            <form class="myForm" id="myForm" method="POST">
-                <input class="inputValue" type="text" id="name" placeholder="Your name">
-                <input class="inputValue" type="text" id="username" placeholder="Your username">
-                <input class="inputValue" type="email" id="email" placeholder="Your email">
-                <input type="submit" value="submit">
-            </form>
-        </div>
-     ` 
-}
-const addUserBtn = document.getElementById('addUserBtn')
-addUserBtn.addEventListener('click', function(){
-    removeElement()
-});
+// function createElement() {
+//     const myBlock = document.createElement('div')
+//     myBlock.classList.add('userBlock')
+//     document.append.append(myBlock)
+//     console.log('misia')
+// }
+// modifBtn.addEventListener('click',createElement)
+// function addNewBlock() {
+//     const title = document.getElementById('myTitle').innerHTML ='Add a new users ?'
+//     document.getElementById('myApp').innerHTML += `
+//         <div class="useradd" id="signUp" style="transition: all 0.9s;">
+//             <form class="myForm" id="myForm" method="POST">
+//                 <input class="inputValue" type="text" id="name" placeholder="Your name">
+//                 <input class="inputValue" type="text" id="username" placeholder="Your username">
+//                 <input class="inputValue" type="email" id="email" placeholder="Your email">
+//                 <input type="submit" value="submit">
+//             </form>
+//         </div>
+//      ` 
+// }
+// const addUserBtn = document.getElementById('addUserBtn')
+// addUserBtn.addEventListener('click', function(){
+//     removeElement()
+// });
 
 
-let nameInput =document.getElementById('name')
-let usernameInput =document.getElementById('username')
-let emailInput =document.getElementById('email')
-let addUserForms = document.getElementById("myForm");
+// let nameInput =document.getElementById('name')
+// let usernameInput =document.getElementById('username')
+// let emailInput =document.getElementById('email')
+// let addUserForms = document.getElementById("myForm");
 
-addUserForms.addEventListener('submit', function(e) {
-    e.preventDefault();
-    sendData();
-    console.log('coucou');
+// addUserForms.addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     sendData();
+//     console.log('coucou');
 
-    const sendData = (data) =>{
-        axios.post('https://jsonplaceholder.typicode.com/users', data)
-        .then(response =>{
-            console.log(response)   
-        })
-        .catch(error =>{
-            console.log(error)
-            getData()
-            createElement()
-        })
-    }
-});
+//     const sendData = (data) =>{
+//         axios.post('https://jsonplaceholder.typicode.com/users', data)
+//         .then(response =>{
+//             console.log(response)   
+//         })
+//         .catch(error =>{
+//             console.log(error)
+//             getData()
+//             createElement()
+//         })
+//     }
+// });
 
 
 
